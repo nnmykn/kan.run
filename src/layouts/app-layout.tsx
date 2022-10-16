a946d6f83739db0665d27130a7842ac6cf669fd0
Ninomiya Kan <ninomiyakan.dev@gmail.com>
2022-10-16 15:33:30 +0900

    無駄なカーソル削除

--- src/layouts/app-layout.tsx
+++ src/layouts/app-layout.tsx
@@ -4,7 +4,6 @@ import { styled } from "solid-styled-components";
 const Container = styled.div`
   background-color: #1a1e2e;
   min-height: 100vh;
-  cursor: url("/1f619-50.png"), auto;
 `;
 
 export const AppLayout: Component<{ children: JSX.Element }> = (props) => {
